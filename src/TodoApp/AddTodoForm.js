91c7c0b91489d8ee2f91d2a9264590be2ee326fb
Kevin Smith <ksmithbaylor@gmail.com>
2015-12-13 21:53:22 -0500

    Implement form with submit handling

--- src/TodoApp/AddTodoForm.js
+++ src/TodoApp/AddTodoForm.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 export default class AddTodoForm extends React.Component {
+  static propTypes = {
+    onSubmit: React.PropTypes.func.isRequired
+  }
+
   render() {
     return (
-      <p>This is the form to add a to-do item</p>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" ref="newTodo"></input>
+        <input type="submit" value="Add"></input>
+      </form>
     );
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.refs.newTodo.value);
   }
 }
